6a62138c2ba037e2979142a2eebd25138ecb7099
ahmed abnaser <ahmeducfjf@gmail.com>
2024-07-28 05:32:13 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls enter student Deptname ");
+            string dept_name = Console.ReadLine();
+            Console.WriteLine("pls enter student Name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("pls enter student id ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("pls enter student age ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"student_Deptname  {dept_name},and his name  {name} ,and his id  {id} ,and his age {age} ");
+
+        }
+    }
+}
